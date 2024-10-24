a1fcd475a1531b72d502098eda13f28f4b7af6a6
Marco <marco.nextime@gmail.com>
2024-10-24 18:47:20 +0100

    Corrigindo estrutura do projeto

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Servir arquivos estáticos da pasta "public"
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+// Rota principal para servir o index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
